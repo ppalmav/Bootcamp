d180cff4a28701adb8e893732eb02216a0a05ad2
Pablo <pablo.palma.v@gmail.com>
2022-11-18 17:50:07 -0300

    until chap 1 promises

--- FT-M3-master/04-AdvancePromises/homework/src/pledge.js
+++ FT-M3-master/04-AdvancePromises/homework/src/pledge.js
@@ -3,8 +3,50 @@
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
+const STATE_PENDING = 'pending'
+const STATE_REJECTED ='rejected'
+const STATE_FULLFILLED ='fulfilled'
+const TYPE_FUNCTION ='function'
+const ERROR_EXECUTOR_NOT_FUNCTION = "Executor is not a function" 
+
+function $Promise(executor){
+    if(typeof executor !== TYPE_FUNCTION){
+        throw new TypeError(ERROR_EXECUTOR_NOT_FUNCTION);
+    }
+    this._state = STATE_PENDING
+    this._value = undefined
+
+    this._internalResolve = (data) => {
+
+        if(this._state===STATE_PENDING){
+            this._state = STATE_FULLFILLED;
+            this._value=data;
+        }
+        //console.log('state en internalresolve='+this._state)
+        return 'Temporarily disabled'
+    }
+
+    this._internalReject = (reason) => {
+
+        if(this._state===STATE_PENDING){
+            this._state = STATE_REJECTED;
+            this._value=reason;
+        }
+        //console.log('state en internalresolve='+this._state)
+        return 'Temporarily disabled'
+    }
+
+    executor(this._internalResolve,this._internalReject)
+}
 
 
+
+
+
+let prom1 = new $Promise( (res,rej) => {
+    
+    return res('promesa1')
+})
 
 module.exports = $Promise;
 /*-------------------------------------------------------
