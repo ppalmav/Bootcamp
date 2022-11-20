80cb63f08eadb276b4b104cf7ee3f74c51e9de3f
Pablo <pablo.palma.v@gmail.com>
2022-11-20 15:43:31 -0300

    parte 2 y 3 advanced promises

--- FT-M3-master/04-AdvancePromises/homework/src/pledge.js
+++ FT-M3-master/04-AdvancePromises/homework/src/pledge.js
@@ -7,7 +7,7 @@ const STATE_PENDING = 'pending'
 const STATE_REJECTED ='rejected'
 const STATE_FULLFILLED ='fulfilled'
 const TYPE_FUNCTION ='function'
-const ERROR_EXECUTOR_NOT_FUNCTION = "Executor is not a function" 
+const ERROR_EXECUTOR_NOT_FUNCTION = "Executor is not a function"
 
 function $Promise(executor){
     if(typeof executor !== TYPE_FUNCTION){
@@ -15,15 +15,16 @@ function $Promise(executor){
     }
     this._state = STATE_PENDING
     this._value = undefined
+    this._handlerGroups = []
 
     this._internalResolve = (data) => {
-
-        if(this._state===STATE_PENDING){
+    
+        if(this._state === STATE_PENDING){
             this._state = STATE_FULLFILLED;
-            this._value=data;
+            this._value = data;
+            this._callHandlers(this._value)
         }
-        //console.log('state en internalresolve='+this._state)
-        return 'Temporarily disabled'
+        
     }
 
     this._internalReject = (reason) => {
@@ -31,22 +32,49 @@ function $Promise(executor){
         if(this._state===STATE_PENDING){
             this._state = STATE_REJECTED;
             this._value=reason;
+            this._callErrorHandlers(this._value)
         }
-        //console.log('state en internalresolve='+this._state)
-        return 'Temporarily disabled'
+    }
+    this._callHandlers = (value) => {
+        while(this._handlerGroups.length>0 && typeof this._handlerGroups[0].successCb === TYPE_FUNCTION) 
+            this._handlerGroups.shift().successCb(value);
+    }
+    this._callErrorHandlers = (value) => {
+        while(this._handlerGroups.length>0 && typeof this._handlerGroups[0].errorCb === TYPE_FUNCTION)
+            this._handlerGroups.shift().errorCb(value);
     }
 
     executor(this._internalResolve,this._internalReject)
+
+    this.then = (succesH,errorH) => {
+        if(typeof succesH !== TYPE_FUNCTION) succesH=false
+        if( typeof errorH !== TYPE_FUNCTION) errorH=false
+        
+        this._handlerGroups.push({successCb:succesH,errorCb:errorH})
+        switch (this._state){
+            case STATE_FULLFILLED:
+                this._callHandlers(this._value)
+                break;
+            case STATE_REJECTED:
+                this._callErrorHandlers(this._value)
+                break;
+            default:
+        }           
+    }
+
+    this.catch = (err) => {
+        this.then(null,err)
+    }
 }
 
 
 
 
 
-let prom1 = new $Promise( (res,rej) => {
+// let prom1 = new $Promise( (res,rej) => {
     
-    return res('promesa1')
-})
+//     return res('promesa1')
+// })
 
 module.exports = $Promise;
 /*-------------------------------------------------------
